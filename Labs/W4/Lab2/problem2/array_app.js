85286ff790184ebdfaccdc2d763e491c0a1f0dc4
JLev189 <jacklevins@hotmail.com>
2022-10-13 18:00:47 +0100

    blocked problem2

--- Labs/W4/Lab2/problem2/array_app.js
+++ Labs/W4/Lab2/problem2/array_app.js
@@ -1,9 +1,9 @@
 //part 1
-let array = fetch('https://jsonplaceholder.typicode.com/posts')
+let response = fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response => response.json())
   .then(json => json.filter(PostTitlesFilter))
   .then(filteredList => filteredList.map(({title}) => ({title})))
-  .then(filteredListTitles => console.log(filteredListTitles))
+//   .then(filteredListTitles => console.log(filteredListTitles))
 
 function PostTitlesFilter(response)  {
     return countWords(response.title) > 6;
@@ -12,3 +12,16 @@ function PostTitlesFilter(response)  {
 function countWords(string) { 
     return string.split(" ").length;  //split string into words array and get its length
 }
+
+//part 2
+let response2 = fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(json => json.map(({body}) => ({body})))
+//   .then(bodyList1 => bodyList1.reduce((words, amount) => {
+//     words[amount.body] = words[amount.body] + 1 || 1;
+//   }))
+//   .then(bodyList => console.log(bodyList))
+//   .then(filteredListTitles => console.log(filteredListTitles))
+
+//get word
+//map(word, reduce(word))
