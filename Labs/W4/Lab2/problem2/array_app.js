6fe7ffa67737e9fd05b870824718170e117c03fb
JLev189 <jacklevins@hotmail.com>
2022-10-14 17:25:48 +0100

    attempt problem2

--- Labs/W4/Lab2/problem2/array_app.js
+++ Labs/W4/Lab2/problem2/array_app.js
@@ -18,29 +18,8 @@ let response2 = fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response => response.json())
   .then(json => json.map(({body}) => ({body})))
   .then(json => json.map(body=> ({
-    word: body.body.split(" "),
-    fre: body.body.split(" ")
+    map: body.body.split(" ").reduce((acc, currentValue) => {  
+      return ({ ...acc, [currentValue]: (acc[currentValue] || 0) + 1 })
+    },[])
   })))
-//   .then(bodyList1 => bodyList1.reduce((words, amount) => {
-//     words[amount.body] = words[amount.body] + 1 || 1;
-//   }))
-  .then(filteredListTitles => console.log( filteredListTitles[0].word.reduce((acc, currentValue) => {
-    console.log(acc);
-    return [...acc, currentValue];
-    // console.log(rec);
-    // console.log(acc[rec]);
-
-    // return ({ ...acc, [rec]: (acc[rec] || 0) + 1 })
-  },[])))
-
-//get word
-//map(word, reduce(word))
-
-
-// let array[a] = b;
-// array[b] = c;
-// var myArray = [];
-// myArray["foo"] = "bar"; //works
-// console.log(myArray["foo"]) //print "bar"
-// myArray["z"] = "baz" //crash with a "RangeError: Invalid array length"
-// console.log(myArray["z"])
+  .then(a => console.log(a))
