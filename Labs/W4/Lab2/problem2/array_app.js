8f2a1f312f31a458ef5d338bad7ce2f8f85f062c
JLev189 <jacklevins@hotmail.com>
2022-10-24 10:59:18 +0100

    add per element and per whole array word freq

--- Labs/W4/Lab2/problem2/array_app.js
+++ Labs/W4/Lab2/problem2/array_app.js
@@ -3,7 +3,7 @@ let response = fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response => response.json())
   .then(json => json.filter(PostTitlesFilter))
   .then(filteredList => filteredList.map(({title}) => ({title})))
-  // .then(filteredListTitles => console.log(filteredListTitles))
+  .then(filteredListTitles => console.log(filteredListTitles))
 
 function PostTitlesFilter(response)  {
     return countWords(response.title) > 6;
@@ -17,9 +17,22 @@ function countWords(string) {
 let response2 = fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response => response.json())
   .then(json => json.map(({body}) => ({body})))
+  /*per array element
   .then(json => json.map(body=> ({
     map: body.body.split(" ").reduce((acc, currentValue) => {  
       return ({ ...acc, [currentValue]: (acc[currentValue] || 0) + 1 })
     },[])
-  })))
+  })))*/
+
+  /* For the whole array */
+  .then(json => json.reduce((acc, currentValue) => {
+    // console.log(acc)
+      return acc.concat(currentValue.body.split(" "))
+    },[]))
+
+
+  .then(json => json.reduce((acc, currentValue) => {
+      return ({ ...acc, [currentValue]: (acc[currentValue] || 0) + 1 })
+    },[])
+  )
   .then(a => console.log(a))
