fbcb6316e140d0e07bef40aaa235b5a86b15a14c
JLev189 <jacklevins@hotmail.com>
2022-11-17 09:16:31 +0000

    setup index.js

---
+++ Labs/W8/Lab4/problem2/src/index.js
@@ -0,0 +1,11 @@
+let hoursInput;
+let minutesInput;
+let secondsInput;
+
+function validateForm(formInput) {
+  hoursInput = formInput.get("hours").trim();
+  minutesInput = formInput.get("minutes").trim();
+  secondsInput = formInput.get("seconds").trim();
+
+  //Validate
+}
