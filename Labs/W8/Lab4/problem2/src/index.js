ceef0860b04635208ea83c86a9a10a9fac70d13b
JLev189 <jacklevins@hotmail.com>
2022-11-17 18:54:02 +0000

    regex validation

--- Labs/W8/Lab4/problem2/src/index.js
+++ Labs/W8/Lab4/problem2/src/index.js
@@ -1,6 +1,8 @@
 let hoursInput;
 let minutesInput;
 let secondsInput;
+const countdownForm = document.getElementById("countdownForm");
+const numberRegex = /^[0-9]*$/;
 
 function validateForm(formInput) {
   hoursInput = formInput.get("hours").trim();
@@ -8,4 +10,26 @@ function validateForm(formInput) {
   secondsInput = formInput.get("seconds").trim();
 
   //Validate
+  if (!numberRegex.test(hoursInput)) {
+    displayError("Hours can only contain numbers");
+    return false; //one error at a time
+  }
+  if (!numberRegex.test(minutesInput)) {
+    displayError("Minutes can only contain numbers");
+    return false; //one error at a time
+  }
+  if (!numberRegex.test(secondsInput)) {
+    displayError("Seconds can only contain numbers");
+    return false; //one error at a time
+  }
 }
+
+countdownForm.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  console.log(ev.target);
+  let formInput = new FormData(countdownForm);
+
+  if (validateForm(formInput)) {
+    //todo if form is valid
+  }
+});
