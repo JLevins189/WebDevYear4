1e50ca0245b0410836fded525b76e0f3f2a38aaf
JLev189 <jacklevins@hotmail.com>
2022-12-12 16:31:36 +0000

    state

--- Labs/W12/problem-1/src/App.js
+++ Labs/W12/problem-1/src/App.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import AddNoteForm from "./AddNoteForm";
 import "./App.css";
 
 function App() {
+  const [noteText, setNoteText] = useState("");
+  const [noteColour, setNoteColour] = useState("white");
+  const [errorMessage, setErrorMessage] = useState({});
+
   const handleAddNote = (e) => {
     e.preventDefault();
     console.log("Test");
   };
+
   return (
     <div className="App">
       <AddNoteForm handleSubmit={handleAddNote} />
