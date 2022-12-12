be7b7c4ae3ff72c46096e65754624de28fe3ad03
JLev189 <jacklevins@hotmail.com>
2022-12-12 17:25:25 +0000

    note template

--- Labs/W12/problem-1/src/NoteListElement.js
+++ Labs/W12/problem-1/src/NoteListElement.js
@@ -1,4 +1,11 @@
-function NoteListElement() {
-  return <div>NoteListElement</div>;
+function NoteListElement(props) {
+  return (
+    <div className="note" style={{ backgroundColor: "red" }}>
+      <p className="notetext"></p>
+      <br />
+      <button>Edit Note</button> &nbsp;
+      <button>Delete Note</button> &nbsp;
+    </div>
+  );
 }
 export default NoteListElement;
