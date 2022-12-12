ea5a0b8cc4e433600c9d1c86ecc90d3407b09cfa
JLev189 <jacklevins@hotmail.com>
2022-12-12 17:20:24 +0000

    note list working

--- Labs/W12/problem-1/src/NoteList.js
+++ Labs/W12/problem-1/src/NoteList.js
@@ -1,10 +1,15 @@
 import EmptyList from "./EmptyList";
+import NoteListElement from "./NoteListElement";
 
 function NoteList(props) {
   const { notes } = props;
   return (
     <div id="notesList">
-      {notes.length > 0 ? <p>Notes not empty</p> : <EmptyList />}
+      {notes.length > 0 ? (
+        notes.map((note) => <NoteListElement />)
+      ) : (
+        <EmptyList />
+      )}
     </div>
   );
 }
---
+++ Labs/W12/problem-1/src/NoteListElement.js
@@ -0,0 +1,4 @@
+function NoteListElement() {
+  return <div>NoteListElement</div>;
+}
+export default NoteListElement;
