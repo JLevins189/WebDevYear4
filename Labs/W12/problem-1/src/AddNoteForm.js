a3080aa141b083073e3a127fd84daeb9ffd37698
JLev189 <jacklevins@hotmail.com>
2022-12-12 16:17:10 +0000

    copy form from notes app

--- Labs/W12/problem-1/src/AddNoteForm.js
+++ Labs/W12/problem-1/src/AddNoteForm.js
@@ -1,7 +1,26 @@
-import React from "react";
-
 function AddNoteForm() {
-  return <div>AddNoteForm</div>;
+  return (
+    <form id="addNoteForm">
+      <label for="note">Note Text:</label>
+      <br />
+      <input type="text" id="note" name="note" />
+      <br />
+
+      <label for="colours">Note Colour:</label>
+      <br />
+      <select name="colours" id="colour-selctor">
+        <option value="white">White</option>
+        <option value="aqua">Aqua</option>
+        <option value="yellowgreen">Green</option>
+        <option value="pink">Pink</option>
+        <option value="yellow">Yellow</option>
+        <option value="darkgray">Grey</option>
+      </select>
+      <br />
+
+      <input type="submit" id="addNoteButton" value="Add Note" />
+    </form>
+  );
 }
 
 export default AddNoteForm;
